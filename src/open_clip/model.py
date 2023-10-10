995320029710d1a632810c7f3cfdb264e4e96aba
mahad852 <mahad852@gmail.com>
2023-10-09 20:57:02 -0400

    using pacl_embedder to get names instead of hard-codding names

--- src/open_clip/model.py
+++ src/open_clip/model.py
@@ -315,9 +315,8 @@ class PACL(CLIP):
         return self.pacl_embedder(tokens)
     
     def lock_all_except_embedder(self):
-        embedder_parameter_names = ["pacl_embedder.fc_res.weight", "pacl_embedder.fc_res.bias", "pacl_embedder.fc1.weight", 
-                                    "pacl_embedder.fc1.bias", "pacl_embedder.fc2.weight", "pacl_embedder.fc2.bias"]
-        
+        embedder_parameter_names = [name for name, param in self.pacl_embedder.named_parameters()]
+                
         for name, param in self.named_parameters():
             if name not in embedder_parameter_names:
                 param.requires_grad = False
