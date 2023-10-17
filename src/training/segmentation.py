fe080afdea27064e2b39fde4ef3b89c0511179c8
Mahad <mahad852@gmail.com>
2023-10-17 13:32:53 -0400

    de-normalizing image before converting back to PIL

--- src/training/segmentation.py
+++ src/training/segmentation.py
@@ -63,11 +63,11 @@ def perform_segmentation(img_path: str, model: PACL, model_name: str):
     mean=getattr(model.visual, 'image_mean')
     std = getattr(model.visual, 'image_std', None)
     original_converter = T.Compose([
-        T.ToPILImage(),
         T.Normalize(
             mean=[-mean[0], -mean[1], -mean[2]],
             std =[1/std[0], 1/std[1], 1/std[2]]
-        )
+        ),
+        T.ToPILImage()
     ])
     img_original = original_converter(img.squeeze(0))
 
